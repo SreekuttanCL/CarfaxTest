79fd462f6d90bbdc6c4a107f398729fa037e6e01
SreekuttanCL <sreekuttancl12@gmail.com>
2021-11-18 00:01:09 -0400

    Initial Commit

---
+++ CarfaxTest/CarfaxTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarfaxTestApp.swift
+//  CarfaxTest
+//
+//  Created by Sreekuttan C L on 2021-11-18.
+//
+
+import SwiftUI
+
+@main
+struct CarfaxTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
